f4243dfdd32aa4e9fb5c9f144fad961813078255
Chirayu-Vyas <114425442+Chirayu-Vyas@users.noreply.github.com>
2023-08-29 18:47:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,110 @@
+import cv2
+import face_recognition
+import math 
+from ultralytics import YOLO
+from ultralytics.yolo.v8.detect import DetectionPredictor
+import cv2 as cv
+import os,sys
+import numpy as np
+
+
+def face_confidence(face_distance, face_match_threshold=0.6):
+    range = (1.0 - face_match_threshold)
+    linear_value = (1.0 - face_distance)/(range*2.0)
+
+    if face_distance>face_match_threshold:
+        return str(round(linear_value*100,2))+'%'
+    else:
+        value = (linear_value + ((1.0- linear_value)* math.pow((linear_value - 0.5)*2,0.2)))* 100
+        return str(round(value,2))+'%'
+
+class Face_and_Object_Recognition: 
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    known_face_encodings = []
+    known_face_names = []
+    process_current_frame = True
+
+    def _init_(self):
+        self.encode_faces()
+        
+        #enode faces
+    def encode_faces(self):
+        for image in os.listdir('faces'):
+            face_images =  face_recognition.load_image_file(f'faces/{image}')
+            face_encoding = face_recognition.face_encodings(face_images)[0]
+
+            self.known_face_encodings.append(face_encoding)
+            self.known_face_names.append(image)
+
+        print(self.known_face_names)
+
+    def recognition(self):
+        video_capture = cv2.VideoCapture(0)
+
+        if not video_capture.isOpened():
+            sys.exit('Video source not found...')
+
+        while True:
+            ret, frame = video_capture.read()
+
+            # model = YOLO("yolov8s.pt")
+
+            # results = model.predict(source =0, show = True)
+
+            # print(*results)
+
+            # Only process every other frame of video to save time
+            if self.process_current_frame:
+                # Resize frame of video to 1/4 size for faster face recognition processing
+                small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+                rgb_small_frame = small_frame[:, :, ::-1]
+
+                # Find all the faces and face encodings in the current frame of video
+                self.face_locations = face_recognition.face_locations(rgb_small_frame)
+                self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
+
+                self.face_names = []
+                for face_encoding in self.face_encodings:
+                    # See if the face is a match for the known face(s)
+                    matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+                    name = "Unknown"
+                    confidence = '  '
+
+                    # Calculate the shortest distance to face
+                    face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+
+                    best_match_index = np.argmin(face_distances)
+                    if matches[best_match_index]:
+                        name = self.known_face_names[best_match_index]
+                        confidence = face_confidence(face_distances[best_match_index])
+
+                    self.face_names.append(f'{name} ({confidence})')
+
+            self.process_current_frame = not self.process_current_frame
+
+            for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 1)
+
+            cv2.imshow('Face Recognition', frame)
+
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+        video_capture.release()
+        cv2.destroyAllWindows()
+
+
+
+fr = Face_and_Object_Recognition()
+fr.recognition()
